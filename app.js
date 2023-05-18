9eacc34db6220441d04c54ebd29d613850facd11
1Eva1987 <115692772+1Eva1987@users.noreply.github.com>
2023-05-18 13:37:14 +0100

    initial files created dependencies installed server is up and running on port 3000

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: "true" }));
+
+app.get("/", (req, res) => {
+  res.send("OK");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
