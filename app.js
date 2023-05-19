0b5e2b8e4649eb8c468247247c2018e5d55394f7
1Eva1987 <115692772+1Eva1987@users.noreply.github.com>
2023-05-19 10:17:47 +0100

    moment installed to easily generate time and date, ejs installed and set to be used to  inject data dynamically into your HTML templates , form with input created values storred in array and and using map method list items created

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const moment = require("moment");
 const app = express();
 
+let item = "";
+let itemsArray = [];
+
 app.use(bodyParser.urlencoded({ extended: "true" }));
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.send("OK");
+  const today = new Date();
+  const currentDate = today.getDay();
+  if (currentDate === 6 || currentDate === 0) {
+    res.render("list", { day: moment.format("LLLL") });
+  } else {
+    res.render("list", {
+      day: moment().format("LLLL"),
+      newListItem: itemsArray,
+    });
+  }
+});
+
+app.post("/", (req, res) => {
+  item = req.body.newItem;
+  itemsArray.push(item);
+  res.redirect("/");
+  console.log(itemsArray);
 });
 
 app.listen(3000, () => {
