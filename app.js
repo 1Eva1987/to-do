1a6d1f80082a47803ba19b8a1944ba613f8b7a7e
1Eva1987 <115692772+1Eva1987@users.noreply.github.com>
2023-05-23 12:40:38 +0100

    work list created using work route in order to check if item belongs to work or home list name and value attributes given to submit button

--- app.js
+++ app.js
@@ -3,26 +3,46 @@ const bodyParser = require("body-parser");
 const moment = require("moment");
 const app = express();
 
-let item = "";
 let itemsArray = [];
+let workItems = [];
 
 app.use(bodyParser.urlencoded({ extended: "true" }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+// home route
+// GET
 app.get("/", (req, res) => {
   res.render("list", {
-    day: moment().format("LLLL"),
+    listTitle: moment().format("LLLL"),
     newListItems: itemsArray,
     as: "labas",
   });
 });
-
+// POST
 app.post("/", (req, res) => {
-  item = req.body.newItem;
-  itemsArray.push(item);
-  res.redirect("/");
-  console.log(itemsArray);
+  console.log(req.body);
+  let item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    itemsArray.push(item);
+    res.redirect("/");
+  }
+});
+
+// work route
+// GET
+app.get("/work", (req, res) => {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+// POST
+app.post("/work", (req, res) => {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
 });
 
 app.listen(3000, () => {
