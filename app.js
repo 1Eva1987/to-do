7f55ce9087dfcac200e62a91b8a7e2d92d10cebd
1Eva1987 <115692772+1Eva1987@users.noreply.github.com>
2023-06-07 13:47:27 +0100

    error handling added

--- app.js
+++ app.js
@@ -9,10 +9,17 @@ const app = express();
 // const workItems = [];
 
 // connecting to mongoDB
-mongoose.connect(process.env.DATABASE_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 
 // creating Shcema
 const itemsShcema = new mongoose.Schema({
